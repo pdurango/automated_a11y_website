3774bf1731680aaa94118591e327ea4fa722827a
TeamDMP <teamdmp@DMP-Student-3.local>
2018-07-20 14:46:14 -0400

    Now displays results html file on website after crawl is executed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Markup
+from creepycrawler import start_crawl
+import sys
 
 app = Flask(__name__)
 
@@ -11,8 +13,14 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    dir_name = str(hash(text) % ((sys.maxsize + 1) * 2))
+    start_crawl.main_execute(dir_name, text, True)
+
+    file_loc = dir_name + "/audits/results-pally.html"
+    result = open(file_loc, "r+")
+    content = Markup(result.read())
+    result.close()
+    return render_template('results.html', results=content)
 
 
 if __name__ == "__main__":
